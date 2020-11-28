33810e09fb4b0e9d29a6074723a67c7d17dd3497
Gabriela Campos <gabbycampos31@gmail.com>
2020-11-28 14:12:18 -0600

    added testing

--- conceptual.md
+++ conceptual.md
@@ -4,29 +4,56 @@ Answer the following questions below:
 
 - What are important differences between Python and JavaScript?
 
+Python runs on servers but not in the browser. JavaScript is a front-end language that
+allows you to create dynamic webpages. 
+
 - Given a dictionary like ``{"a": 1, "b": 2}``: , list two ways you
   can try to get a missing key (like "c") *without* your programming
   crashing.
+  
+  By using the get() method or the setdefault() method.
 
 - What is a unit test?
 
+Unit test is to verify that a single function returns an expected value based on an argument.
+
 - What is an integration test?
 
+Integration test verify that multiple functions are working together properly.
+
 - What is the role of web application framework, like Flask?
+
+Flask help us create web servers. We follow a set of rules for how we should build an application.
+Flask provides us the the tools and libraries that allow us to build a web application easier.. 
 
 - You can pass information to Flask either as a parameter in a route URL
   (like '/foods/pretzel') or using a URL query param (like
   'foods?type=pretzel'). How might you choose which one is a better fit
   for an application?
 
+  It is best to use a query param to filter a search (adding '?') and use a route param for specificity like 
+  /:id
+
 - How do you collect data from a URL placeholder parameter using Flask?
+
+By adding less than and greater sign: <name>
 
 - How do you collect data from the query string using Flask?
 
+By using request.args.get()
+
 - How do you collect data from the body of the request using Flask?
+
+By using request.data
 
 - What is a cookie and what kinds of things are they commonly used for?
 
+A cookie stores data from the browser. Cookies communicate from server to browser and browser to server.
+
 - What is the session object in Flask?
 
+The session object stores data from cookies.
+
 - What does Flask's `jsonify()` do?
+
+It exports JSON files with Flask.
--- test.py
+++ test.py
@@ -11,12 +11,50 @@ class ConvertTests(TestCase):
     def test_home(self):
         """ Test to check that HTML is displayed """
         with self.client:
-            import pdb
-            pdb.set_trace()
+            #import pdb
+            #pdb.set_trace()
 
             res = self.client.get('/')
+            html = res.get_data(as_text=True) # returning text
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('Converting to', html)
+            self.assertIn('Amount', html)
+            self.assertIn('Converting from', html)
+
+    def test_convert(self):
+        """ Test convertion """
+        with self.client:
+            res = self.client.post('/convert', data={'from': 'USD', 'to': 'USD', 'amount': '1'})
+
+            self.assertEqual(res.status_code, 302)
+            self.assertEqual(session['result'], 1)
+            self.assertEqual(session['symbol'], 'US$')
+
+    def test_result(self):
+        """ Test result display """
+        with self.client:
+            with self.client.session_transaction() as sess:
+                sess['result'] = 1
+                sess['symbol'] = '$'
+
+            res = self.client.get('/result')
             html = res.get_data(as_text=True)
 
-            self.assertEqual(response.status_code, 200)
-            # self.assertIn('<label>Converting to</label>', html)
-            # self.assertIn('<label>Amount</label>', html)
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('$ 1', html)
+
+    def test_error(self):
+        with self.client:
+            """ Test error """
+            with self.client.session_transaction() as sess:
+                sess['msg'] = 'This amount is not valid'
+                #sess['sym'] = 'Not a valid currency code: ' # FAILED
+            
+            res = self.client.get('/error')
+            html = res.get_data(as_text=True)
+
+            self.assertEqual(res.status_code, 200)
+            self.assertIn('This amount is not valid', html)
+            #self.assertIn('Not a valid currency code: ', html)
+
